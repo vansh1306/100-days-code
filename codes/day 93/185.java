63f04b01aa0a5244107cc3032e9135076205073b
Vansh Srivastava <167678882+vansh1306@users.noreply.github.com>
2025-10-23 23:59:09 +0530

    Add files via upload

---
+++ codes/day 93/185.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+
+        int count_of_3s = n / 3;
+        int remainder = n % 3;
+
+        if (remainder == 0) {
+            return (int) Math.pow(3, count_of_3s);
+        } else if (remainder == 1) {
+            return (int) Math.pow(3, count_of_3s - 1) * 4;
+        } else {
+            return (int) Math.pow(3, count_of_3s) * 2;
+        }
+    }
+}
